3b8569f1a6cd4b11e27d244cf52082188139905e
Anuragine <anusa2512007@gmail.com>
2025-10-06 22:19:18 +0530

    Add and rename general sharpening kernels and images

--- edge_methods/general_sharpening_kernel2.py
+++ edge_methods/general_sharpening_kernel2.py
@@ -1,9 +1,9 @@
 import cv2
 import numpy as np
 
-kernel = np.array([[0, -1, 0],
-                   [-1, 5, -1],
-                   [0, -1, 0]], dtype=np.float32)  # Sharpen kernel
+kernel = np.array([[-1, -1, -1],
+                   [-1, 9, -1],
+                   [-1, -1, -1]], dtype=np.float32)  # Sharpen kernel
 
 # Read image (grayscale for edge detection/sharpening)
 img = cv2.imread("denoised_ista\denoised_ista_000001.jpeg", cv2.IMREAD_GRAYSCALE)
@@ -12,5 +12,5 @@ img = cv2.imread("denoised_ista\denoised_ista_000001.jpeg", cv2.IMREAD_GRAYSCALE
 filtered_img = cv2.filter2D(img, -1, kernel)
 
 # Save result
-cv2.imwrite("general_sharpening_kernel.png", filtered_img)
+cv2.imwrite("general_sharpening_kernel2.png", filtered_img)
 print("✅ Output saved as output.png")
